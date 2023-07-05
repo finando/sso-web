d2c1ac37e19915beb95949c88ecceefb608d224b
Alexander Zakharov <alexander.zakharov@finando.app>
2023-07-05 21:13:20 +0200

    feat: enable strict mode

--- src/entrypoints/main/index.tsx
+++ src/entrypoints/main/index.tsx
@@ -1,6 +1,7 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './app';
@@ -8,5 +9,9 @@ import App from './app';
 const element = document.getElementById('root');
 
 if (element) {
-  createRoot(element).render(<App />);
+  createRoot(element).render(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
 }
